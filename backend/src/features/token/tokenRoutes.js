36892efd3c0cce85d4d8c2cf6a513a6229747562
souleyFall <souleymanefall2002@gmail.com>
2026-01-12 04:53:55 +0100

    feat: fix for deploy

--- backend/src/features/token/tokenRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenRoutes = void 0;
-const express_1 = require("express");
-const tokenController_1 = require("./tokenController");
-const tokenRouter = (0, express_1.Router)();
-exports.TokenRoutes = tokenRouter;
-tokenRouter.post("/token", tokenController_1.tokenController.generateToken);
--- backend/src/routes/registerRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerRoutes = registerRoutes;
-const text_1 = require("../features/text");
-const token_1 = require("../features/token");
-function registerRoutes(app) {
-    app.use('/api', text_1.TextRoutes);
-    app.use('/api', token_1.TokenRoutes);
-}
